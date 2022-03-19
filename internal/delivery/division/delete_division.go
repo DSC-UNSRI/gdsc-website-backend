3c77e4050222a302e5d7c7a7e577225f92f4f196
Tegar Cahya Bayu Siregar <tegarcahyabayu150502@gmail.com>
2022-03-19 21:39:25 +0700

    Update Delete Division

--- internal/delivery/division/delete_division.go
+++ internal/delivery/division/delete_division.go
@@ -10,11 +10,11 @@ func (handler *divisionHandler) DeleteDivision(ctx *gin.Context) {
 	var req model.DeleteDivisionRequest
 
 	ok := utils.BindURIAndValidate(ctx, &req)
+
 	if !ok {
 		return
 	}
 
 	res := handler.usecase.DeleteDivision(req)
-
 	ctx.JSON(res.Status, res)
 }
--- pkg/utils/request.go
+++ pkg/utils/request.go
@@ -12,31 +12,25 @@ import (
 
 func BindJSONAndValidate(ctx *gin.Context, i interface{}) bool {
 	err := ctx.ShouldBindJSON(i)
+
 	if err == nil {
 		return true
 	}
-
-	if errValidations, ok := err.(validator.ValidationErrors); ok {
-		res := validations.HandleValidationErrors(errValidations)
-		ctx.JSON(res.Status, res)
-	} else if _, ok := err.(*json.UnmarshalTypeError); ok {
-		ctx.JSON(http.StatusBadRequest, model.WebServiceResponse{
-			Message: "Schema request tidak valid",
-			Status:  http.StatusBadRequest,
-			Data:    nil,
-		})
-	} else {
-		ctx.JSON(http.StatusInternalServerError, ToWebServiceResponse("Internal server error", http.StatusInternalServerError, nil))
-	}
-
+	validate(ctx, err)
 	return false
 }
 
 func BindURIAndValidate(ctx *gin.Context, i interface{}) bool {
+
 	err := ctx.ShouldBindUri(i)
 	if err == nil {
 		return true
 	}
+	validate(ctx, err)
+	return false
+}
+
+func validate(ctx *gin.Context, err error) {
 
 	if errValidations, ok := err.(validator.ValidationErrors); ok {
 		res := validations.HandleValidationErrors(errValidations)
@@ -50,6 +44,4 @@ func BindURIAndValidate(ctx *gin.Context, i interface{}) bool {
 	} else {
 		ctx.JSON(http.StatusInternalServerError, ToWebServiceResponse("Internal server error", http.StatusInternalServerError, nil))
 	}
-
-	return false
 }
