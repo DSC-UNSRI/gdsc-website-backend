265228535f6add9b5cf027f82e85bca7619206e9
Tegar Cahya Bayu Siregar <tegarcahyabayu150502@gmail.com>
2022-03-21 09:04:01 +0700

    Re:Update Delete Division

--- pkg/utils/request.go
+++ pkg/utils/request.go
@@ -2,7 +2,6 @@ package utils
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/DSC-UNSRI/gdsc-website-backend/internal/model"
@@ -24,7 +23,6 @@ func BindJSONAndValidate(ctx *gin.Context, i interface{}) bool {
 func BindURIAndValidate(ctx *gin.Context, i interface{}) bool {
 
 	err := ctx.ShouldBindUri(i)
-	fmt.Println("Bind")
 	if err == nil {
 		return true
 	}
@@ -33,20 +31,16 @@ func BindURIAndValidate(ctx *gin.Context, i interface{}) bool {
 }
 
 func validate(ctx *gin.Context, err error) {
-	fmt.Println("Validate")
 	if errValidations, ok := err.(validator.ValidationErrors); ok {
 		res := validations.HandleValidationErrors(errValidations)
-		fmt.Println("Validate 1")
 		ctx.JSON(res.Status, res)
 	} else if _, ok := err.(*json.UnmarshalTypeError); ok {
-		fmt.Println("Validate 2")
 		ctx.JSON(http.StatusBadRequest, model.WebServiceResponse{
 			Message: "Schema request tidak valid",
 			Status:  http.StatusBadRequest,
 			Data:    nil,
 		})
 	} else {
-		fmt.Println("Validate 3")
 		ctx.JSON(http.StatusInternalServerError, ToWebServiceResponse("Internal server error", http.StatusInternalServerError, nil))
 	}
 }
